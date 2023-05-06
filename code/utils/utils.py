edf19fe4a29a75cd1bc566c6c3a5ece7a4b9ae2a
OZON-RUS <rarara106@mail.ru>
2023-05-06 08:29:03 +0300

    Fixed Task.__init__

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -1,24 +1,28 @@
 from datetime import date
 from time import time
 
-
+# Parent class for all the classes of file saving
 class SaveToFile:
     _filename: str
     _obj: object
 
 
+# Parent class for all the classes of database saving
 class SaveToDatabase(SaveToFile):
     pass
 
 
+# Saves the object into SQL database
 class SaveToSQL(SaveToDatabase):
     pass
 
 
+# Saves the object into non SQL database
 class SaveToNonSQL(SaveToDatabase):
     pass
 
 
+# Parent class for all types of Records
 class Record:
 
     def _edit(self, *args, **kwargs):
@@ -27,13 +31,13 @@ class Record:
     def _delete(self):
         pass
 
-
+# Task to be done
 class Task(Record):
-    '''
+    """
     To be implemented:
     subTasks: []
     is_frequent: bool
-    '''
+    """
     priority: int = None
     deadline: date = None
     description: str = None
@@ -42,8 +46,7 @@ class Task(Record):
     time_to_complete: time = None
 
     def __init__(self, *args, **kwargs):
-        #FIX
-        #super(Task, self).__init__(*args, **kwargs)
+        super().__init__()
         for key, value in kwargs.items():
             self.__setattr__(key, value)
         return
