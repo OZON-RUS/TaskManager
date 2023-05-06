503eeac41745e667c98c44534dc633a279c8f786
OZON-RUS <rarara106@mail.ru>
2023-05-06 08:30:06 +0300

    Commented

--- code/console_ui.py
+++ code/console_ui.py
@@ -2,14 +2,23 @@ from console_creator import *
 import controller
 from utils import utils
 
+# Function to create a task
 def create_task():
+
+    # Non-positional arguments fot Task object initialization
     kwargs = {}
+
+    # Loop for getting properties list of Task's class, setting them up and saving it in kwargs
     for var in dir(utils.Task):
         if var.strip('_') == var:
             kwargs[var] = input(f"\t{var}: ")
+
+    # Use controller to connect with core
     task = controller.create_task(**kwargs)
     print(task)
 
+# Console_creator docs:
+# https://github.com/megat69/Lib_ConsoleCreator
 console = Console(exit_aliases=("exit",), lowercase_commands=True, end_message=None)
 
 CREATE_TASK_DESCRIPTION = "This command creates a Task record"
